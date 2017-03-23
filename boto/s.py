17894d3d0bbb29990f14044034f3b83aeb4d5326
Ilan Gavriel <igavriel@users.noreply.github.com>
2017-03-23 13:05:20 +0200

    Update s.py

--- boto/s.py
+++ boto/s.py
@@ -4,12 +4,11 @@ conn = boto.ec2.connect_to_region('us-west-2')
 type(conn)
 
 conn = boto.ec2.connect_to_region('us-west-2',
-    aws_access_key_id = 'AKIAI34R4CELTXMA3TYQ',
-    aws_secret_access_key = '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')
-#aws_access_key_id = AKIAI34R4CELTXMA3TYQ
-#aws_secret_access_key = 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
-#     
+    aws_access_key_id = 'AKTAT34R4CELIXMA3IYQ',
+    aws_secret_access_key = 'otv9xfde/PMu5SG5A+ttKxdAM4QPWh9OIP7p8qoR')
+
 conn.APIVersion
+
 conn.stop_instances(instance_ids=['i-0c63c31167651d9c9'])
 
 #key = s3.get_bucket('media.yourdomain.com').get_key('examples/first_file.csv')
